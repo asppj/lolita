b35098e98b829ce772de8ffbb91013b308a8e6f2
liusp <asppj@foxmail.com>
2020-11-19 16:06:12 +0800

    test: 💍 添加测试

--- test/proto_test.go
+++ test/proto_test.go
@@ -13,12 +13,10 @@ func TestGet(t *testing.T) {
 func TestPost(t *testing.T) {
 	t.Log("测试用例post1")
 }
-<<<<<<< HEAD
 func TestPUT(t *testing.T) {
 	t.Log("测试用例put1")
-=======
+}
 
-func TestPUT(t *testing.T) {
-	t.Log("测试用例put")
->>>>>>> 3a0b57166dc8c91e27b0287c198b4f1142567946
+func TestPATCH(t *testing.T) {
+	t.Log("测试用例patch")
 }
